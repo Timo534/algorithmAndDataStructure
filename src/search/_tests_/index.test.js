a17e2933221c4db23ac5f99894ae609fdf7a8e0a
qiuruiqiang <qiuruiqiang@do1.com.cn>
2020-11-23 18:33:03 +0800

    新增3中二分查找情况： * 查找最后一个值等于给定值的元素 * 查找第一个大于等于给定值的元素 * 查找最后一个小于等于给定值的元素

--- src/search/_tests_/index.test.js
+++ src/search/_tests_/index.test.js
@@ -1,13 +1,74 @@
-const searchEqualValu = require('../index')
+const {
+  searchEqualValue,
+  searchLastEqualValue,
+  searchMoreThanValue,
+  searchLastLessThanValue
+} = require('../index')
 
+// 查找第一个值等于给定值的元素
 describe('test searchEqualValu', () => {
   const arr = [1, 2, 3, 3, 3, 4, 5, 6]
+  // 找到目标元素
   test('exist', () => {
-    const index = searchEqualValu(arr, 3)
+    const index = searchEqualValue(arr, 3)
     expect(index).toBe(2)
   })
+  // 找不到目标元素
   test('no exist', () => {
-    const index = searchEqualValu(arr, 7)
+    const index = searchEqualValue(arr, 7)
+    expect(index).toBe(-1)
+  })
+})
+
+// 查找最后一个值等于给定值的元素
+describe('test searchLastEqualValue', () => {
+  const arr = [1, 2, 3, 3, 3, 4, 5, 6]
+  // 找到目标元素
+  test('exist', () => {
+    const index = searchLastEqualValue(arr, 3)
+    expect(index).toBe(4)
+  })
+  // 找不到目标元素
+  test('no exist', () => {
+    const index = searchLastEqualValue(arr, 7)
+    expect(index).toBe(-1)
+  })
+})
+
+// 查找第一个大于等于给定值的元素
+describe('test searchLastEqualValue', () => {
+  const arr = [1, 2, 3, 3, 3, 4, 5, 6, 8, 8, 8, 9]
+  // 找到目标元素
+  test('exist', () => {
+    const index = searchMoreThanValue(arr, 3)
+    expect(index).toBe(2)
+  })
+  test('more than', () => {
+    const index = searchMoreThanValue(arr, 7)
+    expect(index).toBe(8)
+  })
+  // 找不到目标元素
+  test('no exist', () => {
+    const index = searchMoreThanValue(arr, 10)
+    expect(index).toBe(-1)
+  })
+})
+
+// 查找最后一个小于等于给定值的元素
+describe('test searchLastLessThanValue', () => {
+  const arr = [1, 2, 3, 3, 3, 4, 5, 6, 8, 8, 8, 9]
+  // 找到目标元素
+  test('exist', () => {
+    const index = searchLastLessThanValue(arr, 3)
+    expect(index).toBe(4)
+  })
+  test('more than', () => {
+    const index = searchLastLessThanValue(arr, 4)
+    expect(index).toBe(5)
+  })
+  // 找不到目标元素
+  test('no exist', () => {
+    const index = searchLastLessThanValue(arr, 0)
     expect(index).toBe(-1)
   })
 })
--- src/search/index.js
+++ src/search/index.js
@@ -1,15 +1,66 @@
-module.exports = function searchEqualValu (arr, target) { // target = 3
-  let low = 0, high = arr.length - 1
-  while (low <= high) {
-    let mid = low + ((high - low) >> 1)
-    if (arr[mid] < target) {
-      low = mid + 1
-    } else if (arr[mid] > target) {
-      high = mid - 1
-    } else {
-      if (mid === 0 || arr[mid - 1] !== target) return mid
-      high = mid - 1
+module.exports = {
+  // 查找第一个值等于给定值的元素
+  searchEqualValue: function (arr, target) {
+    let low = 0, high = arr.length - 1
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1)
+      if (arr[mid] < target) {
+        low = mid + 1
+      } else if (arr[mid] > target) {
+        high = mid - 1
+      } else {
+        if (mid === 0 || arr[mid - 1] !== target) return mid
+        high = mid - 1
+      }
     }
+    return -1
+  },
+  // 查找最后一个值等于给定值的元素
+  searchLastEqualValue: function (arr, target) {
+    let low = 0, high = arr.length - 1
+    while (low <= high) {
+      let mid = low + ((high - low) / 2 >> 1)
+      if (arr[mid] < target) {
+        low = mid + 1
+      } else if (arr[mid] > target) {
+        high = mid - 1
+      } else {
+        if (mid === arr.length - 1 || arr[mid + 1] !== target) return mid
+        low = mid + 1
+      }
+    }
+    return -1
+  },
+  // 查找第一个大于等于给定值的元素
+  searchMoreThanValue: function (arr, target) {
+    let low = 0, high = arr.length - 1
+    while (low <= high) {
+      let mid = low + ((high - low) / 2 >> 1)
+      if (arr[mid] < target) {
+        low = mid + 1
+      } else {
+        if (mid === 0 || arr[mid - 1] < target) {
+          return mid
+        }
+        high = mid - 1
+      }
+    }
+    return -1
+  },
+  // 查找最后一个小于等于给定值的元素
+  searchLastLessThanValue: function (arr, target) {
+    let low = 0, high = arr.length - 1
+    while (low <= high) {
+      let mid = low + ((high - low) / 2 >> 1)
+      if (arr[mid] > target) {
+        high = mid - 1
+      } else {
+        if (mid === arr.length - 1 || arr[mid + 1] > target) {
+          return mid
+        }
+        low = mid + 1
+      }
+    }
+    return -1
   }
-  return -1
 }
